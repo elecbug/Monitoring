65063255773d1afdba9d8ea9b7567f8aa83fa647
deveb1479@gmail.com <deveb1479@gmail.com>
2023-10-24 15:55:38 +0900

    edit

--- MonitorX/ListViewColumnSorter.cs
+++ MonitorX/ListViewColumnSorter.cs
@@ -43,14 +43,14 @@ namespace MonitorX
         /// <param name="x">First object to be compared</param>
         /// <param name="y">Second object to be compared</param>
         /// <returns>The result of the comparison. "0" if equal, negative if 'x' is less than 'y' and positive if 'x' is greater than 'y'</returns>
-        public int Compare(object x, object y)
+        public int Compare(object? x, object? y)
         {
             int compareResult;
             ListViewItem listviewX, listviewY;
 
             // Cast the objects to be compared to ListViewItem objects
-            listviewX = (ListViewItem)x;
-            listviewY = (ListViewItem)y;
+            listviewX = (ListViewItem)x!;
+            listviewY = (ListViewItem)y!;
 
             // Compare the two items
             compareResult = ObjectCompare.Compare(listviewX.SubItems[ColumnToSort].Text, listviewY.SubItems[ColumnToSort].Text);
